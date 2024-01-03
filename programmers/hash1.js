44274341c129fc21c90a2e1e0375f1f44eb7262f
Jaehoo Kim <jaehookim@Jaehooui-MacBookPro-2.local>
2024-01-04 00:23:37 +0900

    final

--- programmers/hash1.js
+++ programmers/hash1.js
@@ -8,12 +8,10 @@ function solution(participant, completion) {
 
     participant.forEach((p) => {
         if (join.has(p)) {
-            console.log('join.values(p)', join.values(p));
             join.set(p, +join.get(p) + 1);
         } else {
             join.set(p, 1);
         }
-        console.log('join', join);
     });
 
     completion.forEach((p) => {
@@ -24,10 +22,10 @@ function solution(participant, completion) {
         }
     });
 
-    // console.log(join);
-    console.log(join.keys().next().value);
-
     return join.keys().next().value;
 }
 
 solution(['leo', 'kiki', 'kiki', 'eden'], ['eden', 'leo', 'kiki']);
+
+// 로그를 찍으면 테스트 케이스에서 시간 초과가 난다.
+// 시간복잡도는 O(2N) 일텐데 줄일 수 있는 방법이 있는지는 모르겠다.
