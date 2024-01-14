b7e8539730de0b61ba28df66d0b1ad40aeb9627d
kimvayne <nkimtnt@gmail.com>
2024-01-14 21:04:02 +0900

    fin stack/queue

--- programmers/sq6.js
+++ programmers/sq6.js
@@ -3,32 +3,6 @@
  * https://school.programmers.co.kr/learn/courses/30/lessons/42584
  */
 
-function solution(prices) {
-    const answer = [];
-
-    // 연속했을 경우에만 카운트를 올려야 함
-    // 음 생각을 조금 해봐야겠다!
-
-    prices.forEach((p, idx) => {
-        const sliceP = prices.slice(idx + 1);
-        let num = 0;
-        console.log('sliceP', sliceP);
-
-        sliceP.forEach((sP) => {
-            if (p >= sP) {
-                console.log('p', p);
-                console.log('sP', sP);
-                num += 1;
-                console.log('num', num);
-            }
-        });
-
-        answer.push(num);
-    });
-
-    return answer;
-}
-
 // stack o(n)
 function solution(prices) {
     const answer = Array(prices.length).fill(0);
@@ -36,21 +10,12 @@ function solution(prices) {
 
     for (let i = 0; i < prices.length; i++) {
         while (stack.length > 0 && prices[i] < prices[stack[stack.length - 1]]) {
-            console.log('stack', stack);
-
             const top = stack.pop();
-            console.log('top', top);
-
             answer[top] = i - top;
-            console.log('answer[top]', answer[top]);
         }
-        console.log('before-stack', stack);
-
         stack.push(i);
-        console.log('after-stack', stack);
     }
-
-    console.log('final-stack', stack);
+    
     while (stack.length > 0) {
         const top = stack.pop();
         answer[top] = prices.length - 1 - top;
@@ -59,7 +24,7 @@ function solution(prices) {
     return answer;
 }
 
-console.log('---answer--- : ', solution([1, 2, 3, 2, 3]));
+console.log('---answer--- : ', solution([3, 1, 4, 2, 5]));
 
 // 이중 루프 - o(n2)
 // function solution(prices) {
