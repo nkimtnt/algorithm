28010713dcf3ce5110d3f3301e63005be4252fe4
kimvayne <nkimtnt@gmail.com>
2024-01-31 19:02:52 +0900

    setting

---
+++ backjoon/1003.js
@@ -0,0 +1,13 @@
+/**
+ * 피보나치 함수
+ * DP
+ * https://www.acmicpc.net/problem/1003
+ */
+
+const fs = require('fs');
+const input = fs.readFileSync('./example.txt').toString().trim().split('\n').map(s => +s);
+
+console.log('input', input);
+
+console.log('---answer--- : ', input);
+
