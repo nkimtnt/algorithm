628a1635334c8b8b5ea8eca3343f727674d75b5f
kimvayne <nkimtnt@gmail.com>
2024-02-01 17:57:39 +0900

    no perfect 2 he

--- backjoon/1003.js
+++ backjoon/1003.js
@@ -1,33 +1,33 @@
-const fs = require('fs');
-const input = fs.readFileSync('./example.txt').toString().trim().split('\n').map(s => +s);
-
-const fibo = num => {
-    const memo = new Map();
-
-    const calculateFibo = n => {
-        if (n <= 0) return 0;
-        if (n === 1) return 1;
-
-        if (n >= 2 && memo.has(n)) {
-            return memo.get(n);
-        }
-
-        const result = calculateFibo(n - 1) + calculateFibo(n - 2);
-        memo.set(n, result);
-
-        return result;
-    };
-
-    return calculateFibo(num);
-};
-
-const solution = input => {
-    const uniqueInput = new Set(input);
-    return Array.from(uniqueInput.values()).map(num => {
-        fibo(num);
-        return `${num === 0 ? 1 : fibo(num - 1)} ${fibo(num)}`;
-    }).join('\n');
-};
+// const fs = require('fs');
+// const input = fs.readFileSync('./example.txt').toString().trim().split('\n').map(s => +s);
+//
+// const fibo = num => {
+//     const memo = new Map();
+//
+//     const calculateFibo = n => {
+//         if (n <= 0) return 0;
+//         if (n === 1) return 1;
+//
+//         if (n >= 2 && memo.has(n)) {
+//             return memo.get(n);
+//         }
+//
+//         const result = calculateFibo(n - 1) + calculateFibo(n - 2);
+//         memo.set(n, result);
+//
+//         return result;
+//     };
+//
+//     return calculateFibo(num);
+// };
+//
+// const solution = input => {
+//     const uniqueInput = new Set(input);
+//     return Array.from(uniqueInput.values()).map(num => {
+//         fibo(num);
+//         return `${num === 0 ? 1 : fibo(num - 1)} ${fibo(num)}`;
+//     }).join('\n');
+// };
 
 // 안ㅁ어ㅏ 개빡치네
 // 이 멍청이들 Map 써서 유니크 보장하면되지 ㅋㅋ 했는데
@@ -35,4 +35,20 @@ const solution = input => {
 // 그냥 닥치고 배열 생성해서 인덱스로 메모 해야겠습니다.
 // 속도좀 올려보겠다고 했다가 문제가 -_-....
 
-console.log('---answer--- : ', solution(input));
+const fs = require('fs');
+const stdin = process.platform === 'darwin' ? './example.txt' : '/dev/stdin';
+const num = fs.readFileSync(stdin).toString().trim().split('\n').map(s => +s);
+
+num.shift();
+
+let memo = [[1, 0], [0, 1]];
+
+while (num.length) {
+    const x = num.shift();
+
+    for (let i = memo.length; i <= x; i++) {
+        memo.push([memo[i - 1][0] + memo[i - 2][0], memo[i - 1][1] + memo[i - 2][1]]);
+    }
+
+    console.log(memo[x].join(' '));
+}
