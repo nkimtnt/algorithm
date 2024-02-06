23e46675609bac73be66af5bc3e9907321adcba4
kimvayne <nkimtnt@gmail.com>
2024-02-06 18:57:26 +0900

    ready

--- backjoon/1149.js
+++ backjoon/1149.js
@@ -5,11 +5,29 @@
 
 const fs = require('fs');
 const stdin = process.platform === 'darwin' ? './example.txt' : '/dev/stdin';
-const [tempNum, tempNumArr] = fs.readFileSync(stdin).toString().trim().split('\n');
+const input = fs.readFileSync(stdin).toString().trim().split('\n');
 
-const num = +tempNum;
-const numArr = tempNumArr.split(' ').map(num => +num);
+const num = +input.shift();
+const numArr = input.map(s => s.split(' ').map(s => +s));
+
+// 문제가 원하는건 주어진 num 만큼의 집
+// 각 집마다 RGB 비용
+// N-1, N , N+1 의 색이 겹치지 않는 최소 비용
+
+// 상향 하향 메모
+// 루프 메모 같은데?
+// 뭘 메모? -> 각 자리마다 다음 집과의 합산 비용
+
+console.log('---numArr---', numArr);
+
+const dp = [];
+dp.push(Math.min(...numArr[0]));
+
+console.log('dp', dp);
+
+for (let i = 1; i < numArr.length; i++) {
+    let max;
+}
 
 
-console.log('---answer---', num);
-console.log('---answer---', numArr);
+// console.log(1);
