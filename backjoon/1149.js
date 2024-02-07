27a3b0b91a19a4fbf9e9857c3ac64392061b3f0f
kimvayne <nkimtnt@gmail.com>
2024-02-07 16:38:42 +0900

    done 1149

--- backjoon/1149.js
+++ backjoon/1149.js
@@ -18,16 +18,18 @@ const numArr = input.map(s => s.split(' ').map(s => +s));
 // 루프 메모 같은데?
 // 뭘 메모? -> 각 자리마다 다음 집과의 합산 비용
 
-console.log('---numArr---', numArr);
-
-const dp = [];
-dp.push(Math.min(...numArr[0]));
-
-console.log('dp', dp);
+const dp = [numArr[0]];
 
 for (let i = 1; i < numArr.length; i++) {
-    let max;
+    // dp 에 한칸씩 배열 추가
+    dp.push(Array(3).fill([]));
+
+    // 최솟값을 자신이 속해있지 않은 두 색에서 비교 후 자신과 더해서 최솟값 갱신
+    dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + numArr[i][0];
+    dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + numArr[i][1];
+    dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][0]) + numArr[i][2];
 }
 
+// 마지막 배열에서 최소인 수 고름
+console.log(Math.min(...dp[num - 1]));
 
-// console.log(1);
