6ea154882c98de9a4e5e725aad35fd579dbabf99
kimvayne <nkimtnt@gmail.com>
2024-02-13 16:45:35 +0900

    ready

---
+++ programmers/dfsbfs1.js
@@ -0,0 +1,12 @@
+/**
+ * 타겟 넘버
+ * https://school.programmers.co.kr/learn/courses/30/lessons/43165
+ */
+
+function solution(numbers, target) {
+    var answer = 0;
+    return answer;
+}
+
+console.log('---answer--- : ', solution([1, 1, 1, 1, 1], 3));
+
---
+++ programmers/dfsbfs2.js
@@ -0,0 +1,12 @@
+/**
+ * 게임 맵 최단거리
+ * https://school.programmers.co.kr/learn/courses/30/lessons/1844
+ */
+
+function solution(maps) {
+    var answer = 0;
+    return answer;
+}
+
+console.log('---answer--- : ', solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]], 11));
+
