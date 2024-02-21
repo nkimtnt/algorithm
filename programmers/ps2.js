fec313cadec6369b636981d00b73c04933664a17
kimvayne <nkimtnt@gmail.com>
2024-02-21 11:44:05 +0900

    ready

---
+++ programmers/ps2.js
@@ -0,0 +1,13 @@
+/**
+ * 입국심사
+ * https://school.programmers.co.kr/learn/courses/30/lessons/43236
+ */
+
+function solution(distance, rocks, n) {
+    let answer = 0;
+    
+    return answer;
+}
+
+console.log('---answer--- : ', solution(25, [2, 14, 11, 21, 17], 2));
+
