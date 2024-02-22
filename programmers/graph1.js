c815b191c26e15170b0144cea7e6e75c7fd246b0
kimvayne <nkimtnt@gmail.com>
2024-02-22 16:15:49 +0900

    ready

---
+++ programmers/graph1.js
@@ -0,0 +1,12 @@
+/**
+ * 가장 먼 노드
+ * https://school.programmers.co.kr/learn/courses/30/lessons/49189
+ */
+
+function solution(n, edge) {
+    let answer = 0;
+
+    return answer;
+}
+
+console.log('---answer--- : ', solution(6, [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]));
---
+++ programmers/graph2.js
@@ -0,0 +1,13 @@
+/**
+ * 순위
+ * https://school.programmers.co.kr/learn/courses/30/lessons/49191
+ */
+
+function solution(n, results) {
+    let answer = 0;
+
+    return answer;
+}
+
+console.log('---answer--- : ', solution(25, [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]));
+
